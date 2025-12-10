ad0e4f116dd53b80989ac25b70a069d44365c29d
plaintextspokane <133164421+plaintextspokane@users.noreply.github.com>
2025-12-09 22:17:11 -0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,25 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getAll();
   });
 
+  // Breadcrumb filter
+  eleventyConfig.addNunjucksFilter("breadcrumbs", function(url) {
+    // Remove trailing slash
+    if(url.endsWith("/")) url = url.slice(0, -1);
+
+    // Split path into parts
+    const parts = url.split("/").filter(Boolean);
+
+    // Build breadcrumb HTML
+    let breadcrumb = '<a href="/">Home</a>';
+    let path = "";
+    for(const part of parts) {
+      path += `/${part}`;
+      breadcrumb += ` &raquo; <a href="${path}/">${part}</a>`;
+    }
+
+    return breadcrumb;
+  });
+
   return {
     dir: {
       input: "src",
